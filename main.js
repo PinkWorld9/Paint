89e579b62b84642402c852dd9681a8a9e80ca435
PinkWorld9 <72043702+PinkWorld9@users.noreply.github.com>
2021-01-05 09:29:49 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var canvas = new fabric.Canvas('myCanvas'); 
 player_x = 10; player_y = 10; 
 block_image_width = 30; 
-block_image_height = 30; var player_object= ""; 
+block_image_height = 30; 
+var player_object= ""; 
 var block_image_object= "";
-function player_update() { 
-    fabric.Image.fromURL("player(1).png", 
+function player_update() { fabric.Image.fromURL("player.png", 
     function(Img) { player_object = Img; 
         player_object.scaleToWidth(150); 
         player_object.scaleToHeight(140); player_object.set({ 
@@ -54,4 +54,19 @@ console.log("t"); }
 if(keyPressed == '82') { new_image('thor_right_hand.png'); 
 console.log("r"); }}
 
-
+function up() { if(player_y >=0) { player_y = player_y - block_image_height; 
+    console.log("block image height = " + block_image_height); 
+    console.log("When Up arrow key is pressed, X = " + player_x + " , Y = "+player_y); 
+    canvas.remove(player_object); player_update(); } }
+function down() { if(player_y <=500) { player_y = player_y + block_image_height; 
+    console.log("block image height = " + block_image_height); 
+    console.log("When Down arrow key is pressed, X = " + player_x + " , Y = "+player_y); 
+    canvas.remove(player_object); player_update(); } }
+function left() { if(player_x >0) { player_x = player_x - block_image_width; 
+    console.log("block image width = " + block_image_width); 
+    console.log("When Left arrow key is pressed, X = " + player_x + " , Y = "+player_y); 
+    canvas.remove(player_object); player_update(); } }
+function right() { if(player_x <=850) { player_x = player_x + block_image_width; 
+    console.log("block image width = " + block_image_width); 
+    console.log("When Right arrow key is pressed, X = " + player_x + " , Y = "+player_y); 
+    canvas.remove(player_object); player_update(); } }
